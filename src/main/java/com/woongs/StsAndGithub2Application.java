4d48394baf233007648698b07541d266bc392cea
DESKTOP-2FMNJGO\Woongs <lswlswlsw3@gmail.com>
2018-11-23 11:06:22 +0900

    이렇게 하는거구나 얏호

---
+++ src/main/java/com/woongs/StsAndGithub2Application.java
@@ -0,0 +1,12 @@
+package com.woongs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StsAndGithub2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StsAndGithub2Application.class, args);
+	}
+}
